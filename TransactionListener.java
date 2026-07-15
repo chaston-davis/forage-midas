04af3b54dfb83598781d7a79ba0ba5c2946831ce
Chaston Davis <chastondavis11@aol.com>
2026-07-15 12:28:47 -0500

    Changes2

---
+++ TransactionListener.java
@@ -0,0 +1,12 @@
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void processMessage(Transaction transaction) {
+        System.out.println("Transaction Received: " + transaction);
+    }
+
+}
